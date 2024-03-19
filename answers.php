eff827d7e067faabb573a0549bd4498db4be6592
king <tudor1103@gmail.com>
2024-03-19 15:49:07 +0200

    am modificat answers

--- answers.php
+++ answers.php
@@ -6,7 +6,7 @@ include_once "sys.php";
 $query_questions = "select id_question, question, ans1, ans2, ans3, ans1_cont, ans2_cont, ans3_cont from questions;";
 $questions_data = mysqli_query($link, $query_questions);
 ?>
-
+<?php echo "salut boss"; ?>
 <div class="container-fluid background-img">
     <div class="container-fluid background-img">
         <div class="container">
